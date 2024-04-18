1d10bad057ed0639150574710310af1eb05a1168
0xchamel <pwongexcal@gmail.com>
2024-04-18 20:16:13 +1000

    add config

---
+++ assets/config.js
@@ -0,0 +1,505 @@
+const restPort = location.protocol === 'https:' ? 1318 : 1317;
+const websocketPort = location.protocol === 'https:' ? 26658 : 26657;
+const faucetUbtcPort = location.protocol === 'https:' ? 8001 : 8000;
+const faucetUguuPort = location.protocol === 'https:' ? 8003 : 8002;
+const faucetJpuPort = location.protocol === 'https:' ? 8005 : 8004;
+const faucetUethPort = location.protocol === 'https:' ? 8007 : 8006;
+const faucetEuuPort = location.protocol === 'https:' ? 8009 : 8008;
+
+// const domainCauchyEA = 'a.ununifi-test-v1.cauchye.net';
+const domainCauchyEA = 'gluon-beta.cauchye.net';
+const domainCauchyEB = 'b.ununifi-test-v1.cauchye.net';
+const domainCauchyEC = 'c.ununifi-test-v1.cauchye.net';
+const domainCauchyED = 'd.ununifi-test-v1.cauchye.net';
+const nameCauchyEA = 'Local-test';
+const nameCauchyEB = 'Testnet-B';
+const nameCauchyEC = 'Testnet-C';
+const nameCauchyED = 'Testnet-D';
+
+// const chainID = 'ununifi-test-v1';
+// const chainName = 'UnUniFi (test)';
+const chainID = 'test';
+const chainName = 'UnUniFi (local-test)';
+
+const bech32Prefix = {
+  accAddr: 'ununifi',
+  accPub: 'ununifipub',
+  valAddr: 'ununifivaloper',
+  valPub: 'ununifivaloperpub',
+  consAddr: 'ununifivalcons',
+  consPub: 'ununifivalconspub',
+};
+
+const messageModules = [
+  'bank',
+  'auth',
+  'crisis',
+  'distribution',
+  'evidence',
+  'genaccounts',
+  'gov',
+  'ibc',
+  'slashing',
+  'staking',
+  'auction',
+  'ununifidist',
+  'cdp',
+  'incentive',
+  'pricefeed',
+];
+
+const apps = [
+  { name: 'Utilities', link: '/', icon: 'assistant' },
+  { name: 'NFT Backed Loan', link: '/nft-backed-loan', icon: 'loyalty' },
+  { name: 'Yield Aggregator', link: '/yield-aggregator/vaults', icon: 'pie_chart' },
+  { name: 'Interest Rate Swap', link: '/interest-rate-swap/simple-vaults', icon: 'table_chart' },
+  { name: 'Derivatives', link: '/derivatives/perpetual-futures', icon: 'show_chart' },
+];
+
+const denomMetadata = [
+  {
+    description: 'The governance token of UnUniFi protocol.',
+    denom_units: [
+      {
+        denom: 'uguu',
+        exponent: 0,
+      },
+      {
+        denom: 'guu',
+        exponent: 6,
+      },
+    ],
+    base: 'uguu',
+    name: 'UnUniFi',
+    display: 'GUU',
+    symbol: 'GUU',
+  },
+  {
+    description: 'The first cryptocurrency invented in 2008',
+    denom_units: [
+      {
+        denom: 'ubtc',
+        exponent: 0,
+        aliases: [],
+      },
+      {
+        denom: 'btc',
+        exponent: 6,
+        aliases: [],
+      },
+    ],
+    base: 'ubtc',
+    display: 'BTC',
+    name: 'Bitcoin',
+    symbol: 'BTC',
+  },
+  {
+    description: 'Stablecoin pegged to the ATOM',
+    denom_units: [
+      {
+        denom: 'ustatom',
+        exponent: 0,
+        aliases: [],
+      },
+      {
+        denom: 'statom',
+        exponent: 6,
+        aliases: [],
+      },
+    ],
+    base: 'ustatom',
+    display: 'stATOM',
+    name: 'stATOM',
+    symbol: 'stATOM',
+  },
+  {
+    description: 'Stablecoin pegged to the USD',
+    denom_units: [
+      {
+        denom: 'uusdc',
+        exponent: 0,
+        aliases: [],
+      },
+      {
+        denom: 'uusdc',
+        exponent: 6,
+        aliases: [],
+      },
+    ],
+    base: 'uusdc',
+    display: 'USDC',
+    name: 'USD Coin',
+    symbol: 'USDC',
+  },
+  {
+    description: 'Derivatives Liquidity Provider Token',
+    denom_units: [
+      {
+        denom: 'udlp',
+        exponent: 0,
+        aliases: [],
+      },
+      {
+        denom: 'dlp',
+        exponent: 6,
+        aliases: [],
+      },
+    ],
+    base: 'udlp',
+    name: 'Liquidity Provider',
+    display: 'DLP',
+    symbol: 'DLP',
+  },
+  {
+    description: 'ATOM from Osmosis',
+    denom_units: [
+      {
+        denom: 'ibc/ACBD2CEFAC2CC3ED6EEAF67BBDFDF168F1E4EDA159DFE1CA6B4A57A9CAF4DA11',
+        exponent: 0,
+        aliases: [],
+      },
+    ],
+    base: 'ibc/ACBD2CEFAC2CC3ED6EEAF67BBDFDF168F1E4EDA159DFE1CA6B4A57A9CAF4DA11',
+    name: 'ATOM from Osmosis',
+    display: 'ATOM.osmosis',
+    symbol: 'ATOM',
+  },
+  {
+    description: 'OSMO from Osmosis (deprecated)',
+    denom_units: [
+      {
+        denom: 'ibc/13B2C536BB057AC79D5616B8EA1B9540EC1F2170718CAFF6F0083C966FFFED0B',
+        exponent: 0,
+        aliases: [],
+      },
+    ],
+    base: 'ibc/13B2C536BB057AC79D5616B8EA1B9540EC1F2170718CAFF6F0083C966FFFED0B',
+    name: 'OSMO (deprecated)',
+    display: 'OSMO (deprecated)',
+    symbol: 'OSMO',
+  },
+  {
+    description: 'OSMO from Osmosis',
+    denom_units: [
+      {
+        denom: 'ibc/646315E3B0461F5FA4C5C8968A88FC45D4D5D04A45B98F1B8294DD82F386DD85',
+        exponent: 0,
+        aliases: [],
+      },
+    ],
+    base: 'ibc/646315E3B0461F5FA4C5C8968A88FC45D4D5D04A45B98F1B8294DD82F386DD85',
+    name: 'OSMO',
+    display: 'OSMO',
+    symbol: 'OSMO',
+  },
+];
+
+const strategiesInfo = [
+  {
+    id: '0',
+    denom: 'ibc/13B2C536BB057AC79D5616B8EA1B9540EC1F2170718CAFF6F0083C966FFFED0B',
+    name: 'MARS/OSMO strategy',
+    description: '',
+    gitUrl: '',
+    poolInfo: {
+      type: 'osmosis',
+      poolId: '907',
+    },
+  },
+  {
+    id: '0',
+    denom: 'ibc/646315E3B0461F5FA4C5C8968A88FC45D4D5D04A45B98F1B8294DD82F386DD85',
+    name: 'MARS/OSMO strategy v2',
+    description: '',
+    gitUrl: '',
+    poolInfo: {
+      type: 'osmosis',
+      poolId: '907',
+    },
+  },
+  {
+    id: '1',
+    denom: 'ibc/646315E3B0461F5FA4C5C8968A88FC45D4D5D04A45B98F1B8294DD82F386DD85',
+    name: 'ATOM/OSMO strategy v2',
+    description: '',
+    gitUrl: '',
+    poolInfo: {
+      type: 'osmosis',
+      poolId: '1',
+    },
+  },
+  {
+    id: '0',
+    denom: 'ibc/ACBD2CEFAC2CC3ED6EEAF67BBDFDF168F1E4EDA159DFE1CA6B4A57A9CAF4DA11',
+    name: 'Osmosis ATOM/OSMO Farm',
+    description: '',
+    gitUrl: '',
+    poolInfo: {
+      type: 'osmosis',
+      poolId: '1',
+    },
+  },
+];
+
+const certifiedVaults = ['6', '8', '9', '10', '11'];
+
+const externalChains = [
+  {
+    id: 'cosmoshub',
+    chainId: 'cosmoshub-4',
+    chainName: 'Cosmos Hub',
+    rpc: 'https://rpc-cosmoshub.keplr.app',
+    rest: 'https://lcd-cosmoshub.keplr.app',
+    bip44: { coinType: 118 },
+    bech32Config: {
+      bech32PrefixAccAddr: 'cosmos',
+      bech32PrefixAccPub: 'cosmospub',
+      bech32PrefixConsAddr: 'cosmosvalcons',
+      bech32PrefixConsPub: 'cosmosvalconspub',
+      bech32PrefixValAddr: 'cosmosvaloper',
+      bech32PrefixValPub: 'cosmosvaloperpub',
+    },
+    currencies: [
+      {
+        coinDecimals: 6,
+        coinDenom: 'ATOM',
+        coinGeckoId: 'cosmos',
+        coinMinimalDenom: 'uatom',
+        coinImageUrl:
+          'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/cosmoshub/uatom.png',
+      },
+    ],
+    feeCurrencies: [
+      {
+        coinDecimals: 6,
+        coinDenom: 'ATOM',
+        coinGeckoId: 'cosmos',
+        coinMinimalDenom: 'uatom',
+        coinImageUrl:
+          'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/cosmoshub/uatom.png',
+        gasPriceStep: {
+          average: 0.025,
+          high: 0.03,
+          low: 0.01,
+        },
+      },
+    ],
+    stakeCurrency: {
+      coinDecimals: 6,
+      coinDenom: 'ATOM',
+      coinGeckoId: 'cosmos',
+      coinMinimalDenom: 'uatom',
+      coinImageUrl:
+        'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/cosmoshub/uatom.png',
+    },
+  },
+  {
+    id: 'osmosis',
+    chainId: 'osmosis-1',
+    chainName: 'Osmosis',
+    rpc: 'https://rpc-osmosis.keplr.app',
+    rest: 'https://lcd-osmosis.keplr.app',
+    bip44: { coinType: 118 },
+    bech32Config: {
+      bech32PrefixAccAddr: 'osmo',
+      bech32PrefixAccPub: 'osmopub',
+      bech32PrefixValAddr: 'osmovaloper',
+      bech32PrefixValPub: 'osmovaloperpub',
+      bech32PrefixConsAddr: 'osmovalcons',
+      bech32PrefixConsPub: 'osmovalconspub',
+    },
+    currencies: [
+      {
+        coinDenom: 'OSMO',
+        coinMinimalDenom: 'uosmo',
+        coinDecimals: 6,
+        coinGeckoId: 'osmosis',
+        coinImageUrl:
+          'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/osmosis/uosmo.png',
+      },
+      {
+        coinDenom: 'ION',
+        coinMinimalDenom: 'uion',
+        coinDecimals: 6,
+        coinGeckoId: 'ion',
+        coinImageUrl:
+          'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/osmosis/uion.png',
+      },
+    ],
+    feeCurrencies: [
+      {
+        coinDenom: 'OSMO',
+        coinMinimalDenom: 'uosmo',
+        coinDecimals: 6,
+        coinGeckoId: 'osmosis',
+        coinImageUrl:
+          'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/osmosis/uosmo.png',
+        gasPriceStep: {
+          low: 0.0025,
+          average: 0.025,
+          high: 0.04,
+        },
+      },
+    ],
+    stakeCurrency: {
+      coinDenom: 'OSMO',
+      coinMinimalDenom: 'uosmo',
+      coinDecimals: 6,
+      coinGeckoId: 'osmosis',
+      coinImageUrl:
+        'https://raw.githubusercontent.com/chainapsis/keplr-chain-registry/main/images/osmosis/uosmo.png',
+    },
+  },
+];
+
+const irsVaultsImages = [
+  {
+    contract: 'ununifi1nc5tatafv6eyq7llkr2gv50ff9e22mnf70qgjlv737ktmt4eswrqhp8g9l',
+    image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3794.png',
+    subImage: 'https://s2.coinmarketcap.com/static/img/coins/64x64/21781.png',
+  },
+];
+
+const configs = [
+  // CauchyE A node without Monitor
+  {
+    id: domainCauchyEA,
+    name: nameCauchyEA,
+    restURL: `${location.protocol}//${domainCauchyEA}:${restPort}`,
+    websocketURL: `${location.protocol.replace('http', 'ws')}//${domainCauchyEA}:${websocketPort}`,
+    chainID,
+    chainName,
+    bech32Prefix,
+    minimumGasPrices: [
+      {
+        denom: 'uguu',
+        amount: 0.015,
+      },
+    ],
+    apps,
+    denomMetadata,
+    strategiesInfo,
+    certifiedVaults,
+    externalChains,
+    irsVaultsImages,
+    extension: {
+      faucet: [
+        {
+          hasFaucet: true,
+          faucetURL: `${location.protocol}//${domainCauchyEA}:${faucetUguuPort}`,
+          denom: 'uguu',
+          creditAmount: 2000000000,
+          maxCredit: 2000000000,
+        },
+      ],
+      monitor: undefined,
+      navigations: [],
+      messageModules,
+    },
+  },
+  // // CauchyE B node without Monitor
+  // {
+  //   id: domainCauchyEB,
+  //   name: nameCauchyEB,
+  //   restURL: `${location.protocol}//${domainCauchyEB}:${restPort}`,
+  //   websocketURL: `${location.protocol.replace('http', 'ws')}//${domainCauchyEB}:${websocketPort}`,
+  //   chainID,
+  //   chainName,
+  //   bech32Prefix,
+  //   minimumGasPrices: [
+  //     {
+  //       denom: 'uguu',
+  //       amount: 0.015,
+  //     },
+  //   ],
+  //   apps,
+  //   denomMetadata,
+  //   strategiesInfo,
+  //   certifiedVaults,
+  //   externalChains,
+  //   extension: {
+  //     faucet: [
+  //       {
+  //         hasFaucet: true,
+  //         faucetURL: `${location.protocol}//${domainCauchyEB}:${faucetUguuPort}`,
+  //         denom: 'uguu',
+  //         creditAmount: 2000000000,
+  //         maxCredit: 2000000000,
+  //       },
+  //     ],
+  //     monitor: undefined,
+  //     navigations: [],
+  //     messageModules,
+  //   },
+  // },
+  // // CauchyE C node without Monitor
+  // {
+  //   id: domainCauchyEC,
+  //   name: nameCauchyEC,
+  //   restURL: `${location.protocol}//${domainCauchyEC}:${restPort}`,
+  //   websocketURL: `${location.protocol.replace('http', 'ws')}//${domainCauchyEC}:${websocketPort}`,
+  //   chainID,
+  //   chainName,
+  //   bech32Prefix,
+  //   minimumGasPrices: [
+  //     {
+  //       denom: 'uguu',
+  //       amount: 0.015,
+  //     },
+  //   ],
+  //   apps,
+  //   denomMetadata,
+  //   strategiesInfo,
+  //   certifiedVaults,
+  //   externalChains,
+  //   extension: {
+  //     faucet: [
+  //       {
+  //         hasFaucet: true,
+  //         faucetURL: `${location.protocol}//${domainCauchyEC}:${faucetUguuPort}`,
+  //         denom: 'uguu',
+  //         creditAmount: 2000000000,
+  //         maxCredit: 2000000000,
+  //       },
+  //     ],
+  //     monitor: undefined,
+  //     navigations: [],
+  //     messageModules,
+  //   },
+  // },
+  // // CauchyE D node without Monitor
+  // {
+  //   id: domainCauchyED,
+  //   name: nameCauchyED,
+  //   restURL: `${location.protocol}//${domainCauchyED}:${restPort}`,
+  //   websocketURL: `${location.protocol.replace('http', 'ws')}//${domainCauchyED}:${websocketPort}`,
+  //   chainID,
+  //   chainName,
+  //   bech32Prefix,
+  //   minimumGasPrices: [
+  //     {
+  //       denom: 'uguu',
+  //       amount: 0.015,
+  //     },
+  //   ],
+  //   apps,
+  //   denomMetadata,
+  //   strategiesInfo,
+  //   certifiedVaults,
+  //   externalChains,
+  //   extension: {
+  //     faucet: [
+  //       {
+  //         hasFaucet: true,
+  //         faucetURL: `${location.protocol}//${domainCauchyED}:${faucetUguuPort}`,
+  //         denom: 'uguu',
+  //         creditAmount: 2000000000,
+  //         maxCredit: 2000000000,
+  //       },
+  //     ],
+  //     monitor: undefined,
+  //     navigations: [],
+  //     messageModules,
+  //   },
+  // },
+];
